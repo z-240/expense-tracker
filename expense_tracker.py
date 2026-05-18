d020979792676303d1c9dbf586a25a959262d801
Zaynudiin Abdi <zayn_2008@outlook.com>
2026-05-18 01:36:56 +0100

    Added expenses input and storage

--- expense_tracker.py
+++ expense_tracker.py
@@ -5,12 +5,24 @@ def show_menu(): # we use a function rather than multiple prints so this can be 
     print("3. Show total spending")
     print("4. Exit")
 
+expenses = []
+
+def add_expense():
+     name = input("Enter expense name: ")
+     amount = float(input("Enter expense amount: "))
+     expense = {
+          "name": name,
+          "amount": amount
+     }
+     expenses.append(expense)
+     print("Expense added successfully!")
+
 while True: # this creates an infinite loop that will keep showing the menu until the user chooses to exit.
         show_menu() # call the function to display the menu options
         choice = input("Choose an option: ")
 
         if choice == "1":
-            print("Add expense selected")
+            add_expense() # call the function to add an expense
         elif choice == "2":
             print("View expenses selected")
         elif choice == "3":
